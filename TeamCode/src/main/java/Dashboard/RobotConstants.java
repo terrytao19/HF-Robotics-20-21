d2b7934ba3f3e1655c31c9120c1df5150521aff9
jpc405 <61483663+jpc405@users.noreply.github.com>
2021-02-09 16:25:55 -0500

    Worked on drive ModuleSide

--- TeamCode/src/main/java/Dashboard/RobotConstants.java
+++ TeamCode/src/main/java/Dashboard/RobotConstants.java
@@ -1,4 +1,33 @@
 package Dashboard;
+import com.acmerobotics.dashboard.config.Config;
 
+@Config
 public class RobotConstants {
+
+    //target - current is your error
+    //and to send power proportional to error
+    //just multiply it by a tuned constant
+    //kP
+    //so double pControl = kP * error;
+//error is setpoint - current, and then your power is just kP * error + kI * integral(error) + kD * deriv(error)
+//integral of error is just a running sum of error, and derivative of error is just the rate of change of error
+    public static double GYRO_TURN_KP = 0.03;
+    public static double GYRO_TURN_KI = 0;
+    public static double GYRO_TURN_KD = 0.003;
+
+    public static double Straight_KP = .08;
+    public static double Straight_KI = 0;
+    public static double Straight_KD = 0;
+    public static double Target_Angle = 90;
+
+    public static double MAX_VELOCITY = 27.5;
+    public static double MAX_ACCELERATION = 20;
+    public static double DRIVE_GEAR_RATIO = 1.387755102;
+    public static double STEERING_GEAR_RATIO = 3.4285714286;
+
+    public static double MOTOR_1 = 1;
+    public static double MOTOR_2 = -1;
+    public static double MOTOR_3 = 1;
+    public static double MOTOR_4 = -1;
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TBertOpmodes/BertLinearOpMode.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.TBertOpmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Misc.PID;
+import org.firstinspires.ftc.teamcode.RobotStuff.Hardware.HardwareBert;
+import org.firstinspires.ftc.teamcode.RobotStuff.diffyswerve.DriveModule;
+
+import Dashboard.RobotConstants;
+
+public class BertLinearOpMode extends OpMode {
+    public HardwareBert robot;
+   FtcDashboard dashboard = FtcDashboard.getInstance();
+   TelemetryPacket packet = new TelemetryPacket();
+
+  // ElapsedTime time = new ElapsedTime();
+
+   // public void initHardware(HardwareBert robot){
+    //    this.robot = robot;
+    //}
+
+
+    // represents 1 loop of correction PID loop. Called repeatedly until the target is reached
+   /* public double PIDStraightCorrection(double current, double target, PID pid){
+        double error = target - current;
+        //if the error is greater than 180, we know it has to cross the 180 -180 boundary, so we switch to a 0-360 scale
+        //this is called an "angle wrap"
+        if(Math.abs(error) > 180){
+            current = (current + 360) % 360;
+            target = (target + 360) % 360;
+        }
+
+        //resets the target in case it's changed
+        pid.setTarget(target);
+
+        //does calculations using PID controller
+        pid.updatePID(current);
+
+        //gets the output value, which is set in the method updatePID()
+        return pid.getOutput();
+    }
+    */
+    DcMotor leftMotor1;
+    DcMotor leftMotor2;
+    DcMotor rightMotor1;
+    DcMotor rightMotor2;
+
+    @Override
+    public void init() {
+        leftMotor1 = hardwareMap.dcMotor.get("left1");
+        leftMotor2 = hardwareMap.dcMotor.get("left2");
+        rightMotor1 = hardwareMap.dcMotor.get("right1");
+        rightMotor2 = hardwareMap.dcMotor.get("right2");
+    }
+
+    public void loop(){
+        leftMotor1.setPower(RobotConstants.MOTOR_1);
+        leftMotor2.setPower(RobotConstants.MOTOR_2);
+        rightMotor1.setPower(RobotConstants.MOTOR_3);
+        rightMotor2.setPower(RobotConstants.MOTOR_4);
+
+
+    }
+
+}
