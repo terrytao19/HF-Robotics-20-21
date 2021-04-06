9c0936d2036c466513cb001d59a7d8a61d848dd0
terry <terrytao19@gmail.com>
2021-04-06 16:24:08 -0400

    Created HardwareBert Created BertTeleOPp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/motortest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+//beyblade
+@Disabled
+@TeleOp
+public class motortest  extends OpMode {
+    DcMotor motorleft;
+
+
+    public void init() {
+
+        motorleft = hardwareMap.dcMotor.get("motor0");
+    }
+    public void init_loop(){}
+
+    public void start(){}
+
+
+    public void loop() {
+        motorleft.setPower(1);
+
+    }
+}
