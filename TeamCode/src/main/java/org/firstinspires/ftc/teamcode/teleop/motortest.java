8efb3d7c1a0664771070e690a37c788382a0704a
terry <terrytao19@gmail.com>
2021-04-06 16:24:27 -0400

    Created HardwareBert Created BertTeleOPp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/motortest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/motortest.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//beyblade
+//beyblad
 @Disabled
 @TeleOp
 public class motortest  extends OpMode {
